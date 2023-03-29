9b6c55ce65090c14a59fda3a4206f143c9d5259d
parindagade <124175099+parindagade@users.noreply.github.com>
2023-03-29 13:54:28 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import 'zone.js/dist/zone';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from 'app.module';
+import { AppModule } from './src/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
